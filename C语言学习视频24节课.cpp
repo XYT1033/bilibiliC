95f4b97fcb61a1c5271433999bb6da712424a390
git_global <git@global.com>
2019-05-26 14:35:52 +0800

    1

--- C语言学习视频24节课.cpp
+++ C语言学习视频24节课.cpp
@@ -10,17 +10,46 @@
   
   
 
+
+
 #include <stdio.h>
 #include <tchar.h>
 
 #define PI 3.1415  // 如果改为3又是一个int 类型 ，不能 检测类型错误 
+#define FUN(X,Y) ((X)*(Y))
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int pi = 3.434222343343;  // 能 检测类型错误
 	printf("PI=%lf,pi=%f\n",PI,pi);
+	int q = FUN(10+10,20+10);
+
+	printf("PI=%lf,pi=%f q=%d\n",PI,pi,q);
+	printf("__line__ = %d  ,  __file__=%s\n", __LINE__, __FILE__);
 
 	getchar();
 	return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
